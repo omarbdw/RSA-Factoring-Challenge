df3a4ebe988b0820a24873600974a96d6d45b3db
omarbdw <omarbdw@yahoo..com>
2023-11-18 22:03:58 +0200

    RSA1.1

--- main.py
+++ main.py
@@ -2,8 +2,21 @@
 import sys
 
 def read_numbers_from_file(filename):
-    with open(filename, 'r') as file:
-        numbers = [int(line.strip()) for line in file]
+    #check if file exists
+    try:
+        f = open(filename)
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+    #check if file is empty
+    if f.readline() == "":
+        print("File is empty")
+        sys.exit(1)
+    #read numbers from file
+    with open(filename, "r") as f:
+        numbers = []
+        for line in f:
+            numbers.append(int(line))
     return numbers
 
 def getFactors(number):
@@ -14,6 +27,10 @@ def getFactors(number):
     return p, q
 
 if __name__ == "__main__":
+    #check if filename is given
+    if len(sys.argv) < 2:
+        print("Usage: main.py <filename>")
+        sys.exit(1)
     filename = sys.argv[1]
     numbers = read_numbers_from_file(filename)
     for number in numbers:
